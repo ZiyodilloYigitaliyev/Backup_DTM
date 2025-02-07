eef37c43819d3a0b4397e83331a1979409b01950
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2025-02-07 04:51:00 -0800

    👍

--- Backup/models.py
+++ Backup/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class ProcessedID(models.Model):
-    list_id = models.IntegerField(null=True, blank=True)
+    list_id = models.IntegerField(null=True, default=0, blank=True)
 
 class Mapping_Data(models.Model):
     true_answersID = models.ForeignKey(ProcessedID, null=True, blank=True, on_delete=models.CASCADE)
