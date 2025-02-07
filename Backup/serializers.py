ac33009af3e187d1681df0897a82d46ed81a9421
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2025-02-07 04:40:03 -0800

    👍

--- Backup/serializers.py
+++ Backup/serializers.py
@@ -1,7 +1,12 @@
-from .models import Mapping_Data
+from .models import Mapping_Data, ProcessedID
 from rest_framework import serializers
 
 class MappingDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mapping_Data
+        fields = '__all__'
+
+class ProcessedIDSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProcessedID
         fields = '__all__'
