801cd528bf0aa85f650883cc7f178d5f2004f4ed
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2025-02-07 06:22:11 -0800

    👍

--- Backup/models.py
+++ Backup/models.py
@@ -8,5 +8,5 @@ class ProcessedID(models.Model):
 class Mapping_Data(models.Model):
     true_answersID = models.ForeignKey(ProcessedID, null=True, blank=True, on_delete=models.CASCADE)
     category = models.CharField(max_length=200, null=True, blank=True)
-    true_answer = models.CharField(max_length=20, null=True, blank=True)
+    true_answer = models.CharField(max_length=255, null=True, blank=True)
     order = models.IntegerField(null=True, blank=True)
