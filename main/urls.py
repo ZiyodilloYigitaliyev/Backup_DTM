9e4f34da4813ceb8f582b4824de20ea49b0e2be1
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2025-02-08 19:46:48 -0800

    👍

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ProcessDataView
 
 urlpatterns = [
-   path("upload/", ProcessDataView, name="ProcessDataView"),
+   path("upload/",  ProcessDataView.as_view(), name="upload"),
 ]
