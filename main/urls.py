67ba3bfe4fc97f2d38747f4d7d970dbc13eead53
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2025-02-18 08:31:41 -0800

    👍

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ResultDataCreateAPIView, PDFResultRetrieveAPIView
+from .views import ResultDataCreateAPIView, PDFResultRetrieveAPIView, ResultDataByDateRetrieveAPIView
 
 urlpatterns = [
    path("upload/",  ResultDataCreateAPIView.as_view(), name="upload"),
    path("get-pdf/",  PDFResultRetrieveAPIView.as_view(), name="get-pdf"),
+   path("get-result/" ResultDataByDateRetrieveAPIView.as_view(), name="get-result_by_date")
 ]
