1eff7e28ad8c6f2f2e4c1f54eb6e0748784394b9
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2025-02-18 08:43:32 -0800

    👍

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,5 @@ from .views import ResultDataCreateAPIView, PDFResultRetrieveAPIView, ResultData
 urlpatterns = [
    path("upload/",  ResultDataCreateAPIView.as_view(), name="upload"),
    path("get-pdf/",  PDFResultRetrieveAPIView.as_view(), name="get-pdf"),
-   path("get-result/" ResultDataByDateRetrieveAPIView.as_view(), name="get-result_by_date")
+   path("get-result/" ResultDataByDateRetrieveAPIView.as_view(), name="get-result")
 ]
